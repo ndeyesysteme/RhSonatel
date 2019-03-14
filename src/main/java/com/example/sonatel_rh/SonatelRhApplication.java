ef7df97569844343a034a95d5a28ad1ce5fb40c4
diop ndeye ami <diopndeyeamie@gmail.com>
2019-03-14 11:47:02 +0000

    first commit

---
+++ src/main/java/com/example/sonatel_rh/SonatelRhApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonatel_rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonatelRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonatelRhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sonatel_rh/SonatelRhApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sonatel_rh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SonatelRhApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
